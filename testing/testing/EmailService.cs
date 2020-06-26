1cebb96cb681e3dfd71d16cfbc666b0c9d3756b8
Nicolas Pockaj <NPockaj@medicalcardsystem.com>
2020-06-26 14:35:24 -0400

    todo

---
+++ testing/testing/EmailService.cs
@@ -0,0 +1,57 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing
+{
+    class EmailService
+    {
+        
+            public static async Task Execute(string mailTo, string subject, string body, string mailFrom, string mailName, Dictionary<string, string> args = null)
+            {
+
+                try
+                {
+                    if (string.IsNullOrEmpty(mailTo)) return;
+
+
+
+                    var settings = ConfigurationManager.AppSettings;
+                    List<EmailAddress> addresses = new List<EmailAddress>();
+
+                    var mailAddressList = mailTo.Split(';').ToList();
+                    foreach (string address in mailAddressList)
+                        addresses.Add(new EmailAddress(address));
+
+                    var sendGridMail = mailFrom;
+                    var sendGridMailName = mailName;
+                    var smtpPassword = settings["SmtpPassword"];
+
+                    var apiKey = smtpPassword;
+                    var client = new SendGridClient(apiKey);
+                    var msg = new SendGridMessage()
+                    {
+                        From = new EmailAddress(sendGridMail, sendGridMailName),
+                        Subject = subject,
+                        HtmlContent = $@"{body}"
+                    };
+
+                    msg.AddTos(addresses);
+
+
+
+                    var response = await client.SendEmailAsync(msg);
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+        
+    }
+}
---
+++ testing/testing/Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driverService = ChromeDriverService.CreateDefaultService();
+            ChromeOptions options = new ChromeOptions();
+            //      options.AddArgument("--no-startup-window");
+
+            IWebDriver driver = new ChromeDriver(driverService, options);
+            driver.Manage().Window.Position = new System.Drawing.Point(-2000, 0);
+
+
+            driver.Navigate().GoToUrl("https://www.mcs.com.pr/");
+
+            IWebElement img = driver.FindElement(By.Id("imgPrefetch"));
+
+            driver.Quit();
+
+
+        }
+    }
+}
